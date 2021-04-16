385be8092f1bf81f177a6a8b7df114d6bfec608d
Shamil <badalloffsh@gmail.com>
2021-04-16 14:23:17 +0200

    Add: pokemon component and get-pokemons qraphql query

---
+++ src/components/Pokemon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function Pokemon({ pokemon }) {
+    return (
+        <div className="pokemon">
+            <div className="pokemon__name">
+                <p>{pokemon.name}</p>
+            </div>
+            <div className="pokemon__meta">
+                <span>{pokemon.maxHP}</span>
+                <span>{pokemon.maxCP}</span>
+            </div>
+            <div className="pokemon__image">
+                <img src={pokemon.image} alt={pokemon.name} />
+            </div>
+            <div className="pokemon__attacks">
+            
+            </div>
+        </div>
+    )
+}
--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Pokemon } from '../components/Pokemon';
+import {GET_POKEMONS} from '../graphql/get-pokemons'
 
 export function PokemonsContainer() {
+    const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+        variables: { first: 9 },
+    });
+    
     return (
-        <p>I am a pokemon container</p>  
+        <div className="container">
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        </div>
     );
 }
